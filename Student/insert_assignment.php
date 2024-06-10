a68a491eb986128f52bc96b4a238768239a0408a
Haiqal Bin Ashraf <tp070556@mail.apu.edu.my>
2024-06-10 13:41:52 +0000

    git commit -m "Student Dashboard" git push origin haiqal ```

--- Student/insert_assignment.php
+++ Student/insert_assignment.php
@@ -1,5 +1,5 @@
 <?php
-require_once("class/location");
+require_once("class/student_submit.php");
 if($_FILES['file']) {
     $assignment = new UploadFile();
     $assignment->attach_file($_FILES['main_picture'], $_POST['name'], $_POST['description']);
